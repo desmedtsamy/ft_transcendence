eff90773deae904ab666c299f07dbe6bc67491de
sde-smed <sde-smed@student.s19.be>
2024-12-03 16:00:58 +0100

    part 2

--- srcs/django/pong/services.py
+++ srcs/django/pong/services.py
@@ -1,10 +1,10 @@
 # services.py
 
-def getMatch(match_id):
-	try:
-		match = Match.objects.get(id=match_id)
-		return {'status': 'success', 'match': match}, 200
-	except Match.DoesNotExist:
-		return {'error': 'Match introuvable'}, 404
-	except Exception as e:
-		return {'error': str(e)}, 500
+# def getMatch(match_id):
+# 	try:
+# 		match = Match.objects.get(id=match_id)
+# 		return {'status': 'success', 'match': match}, 200
+# 	except Match.DoesNotExist:
+# 		return {'error': 'Match introuvable'}, 404
+# 	except Exception as e:
+# 		return {'error': str(e)}, 500
